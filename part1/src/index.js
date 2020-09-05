2ec65a70bf7bf254236019253f77a184eb45c574
jnrdrgz <jnigrodriguez@gmail.com>
2020-09-05 18:27:44 -0300

    state complex2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,32 +1,53 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const History = (props) => {
+  if(props.allClicks.length === 0){
+    return (
+        <div>
+            the app is used bt pressing the buttons
+        </div>
+    )
+  }
 
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick = {onClick}>
+    {text}
+  </button>
+)
 
 const App = (props) => {
-  const [clicks, setClicks] = useState({
-    left:0,right:0
-  })
-
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  
   const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1
-    }
-
-    setClicks(newClicks)
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
   }
+  
   const handleRightClick = () => {
-    setClicks({...clicks,right: clicks.right + 1})
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
   }
 
   return (
     <div>
       <div>
-        {clicks.left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
-        {clicks.right}
+        {left}
+        <Button onClick={handleLeftClick} text="left" />
+        <Button onClick={handleRightClick} text="right" />
+        {right}
+
+      <History allClicks={allClicks} />
+      
       </div>
     </div>
   );
