0ed93e7e169f663181368cebd127b0794f20e9ba
jnrdrgz <jnigrodriguez@gmail.com>
2020-09-06 18:05:13 -0300

    1.11*: unicafe

--- part1-ej1/src/index.js
+++ part1-ej1/src/index.js
@@ -1,64 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (course) => {
-  return (
-    <h1>{course.course}</h1>
+
+const Button = ({onClick, text}) => (
+  <button onClick = {onClick}>
+    {text}
+  </button>
+)
+
+const Statistic = ({text, value}) => {
+  return(
+    <tr><td>{text}</td><td>{value}</td> <br /></tr>
   )
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  const _all = bad+neutral+good
+  const average = (good+bad*-1)/_all
+  const positive_p = (good*100)/_all + "%"
+  
+  if(_all === 0){
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Tatistics</h1>
+        <table>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={_all} />
+          <Statistic text="average" value={average} />
+          <Statistic text="positive" value={positive_p} />
+        </table>
+      </div>
+    )
+  }
 
-const Part = (part) => {
-  return (
-    <div>
-      <p>
-        part: {part.name} exercises: {part.exercises}
-      </p>
-    </div>
-  )
+
 }
 
-const Content = (parts) => {
-  console.log(parts)
-  return (
-    <div>
-    <Part name={parts.parts[0].name} exercises={parts.parts[0].exercises}/>
-    <Part name={parts.parts[1].name} exercises={parts.parts[1].exercises}/>
-    <Part name={parts.parts[2].name} exercises={parts.parts[2].exercises}/>
-      
-    </div>
-  )
 
-}
-
-const Total = (exercises) => {
-  console.log(exercises.exercises)
-  return(<div>
-    <p>Number of exercises {exercises.exercises.reduce((a, b) => a + b, 0)}</p>
-  </div>)
-}
-
-//exercises.reduce((a, b) => a + b, 0)
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  var parts = [{name:part1,exercises:exercises1},{name:part2,exercises:exercises2},{name:part3,exercises:exercises3}]
-      
+  const [allOpinions, setAll] = useState({})
+  
+
+  const handler = (_state, _setter) => {
+    let f = () => { _setter(_state+1) }
+    return f
+  }
+
+  const handleBadClick = handler(bad,setBad)
+  const handleGoodClick = handler(good,setGood)
+  const handleNeutralClick = handler(neutral,setNeutral)
+  
+  //const handleNeutralClick = () => {
+  //  //setAll(allClicks.concat('L'))
+  //  setNeutral(neutral+1)
+  //}
+  //const handleGoodClick = () => {
+  //  setGood(good+1)
+  //}
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total exercises={[exercises1,exercises2,exercises3]}/>
+      <div>
+        <h1>Give Feedback</h1>
+
+        <Button onClick={handleGoodClick} text="Good" />
+        <Button onClick={handleNeutralClick} text="Neutral" />
+        <Button onClick={handleBadClick} text="Bad" />
+
+        <Statistics bad={bad} neutral={neutral} good={good}/>
+      </div>
     </div>
+    
   )
 }
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
