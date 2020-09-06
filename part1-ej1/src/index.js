aa608633f63a00795954ee788bbbb8b05fe1f28c
jnrdrgz <jnigrodriguez@gmail.com>
2020-09-06 19:04:07 -0300

    1.14: anecdotes step3

--- part1-ej1/src/index.js
+++ part1-ej1/src/index.js
@@ -1,91 +1,81 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
 const Button = ({onClick, text}) => (
   <button onClick = {onClick}>
     {text}
   </button>
 )
 
-const Statistic = ({text, value}) => {
-  return(
-    <tr><td>{text}</td><td>{value}</td> <br /></tr>
-  )
-}
-
-const Statistics = ({good, neutral, bad}) => {
-  const _all = bad+neutral+good
-  const average = (good+bad*-1)/_all
-  const positive_p = (good*100)/_all + "%"
+const Anecdotes = () => {
+  const max_point_index = points.indexOf(Math.max(...points))
+  console.log(max_point_index)
   
-  if(_all === 0){
+  if(Math.max(...points) === 0){
     return (
       <div>
-        No feedback given
+      <h1>Anecdote with most votes</h1>
+      <p>all anectodetes has 0 points</p>
       </div>
     )
   } else {
     return (
       <div>
-        <h1>Tatistics</h1>
-        <table>
-          <Statistic text="good" value={good} />
-          <Statistic text="neutral" value={neutral} />
-          <Statistic text="bad" value={bad} />
-          <Statistic text="all" value={_all} />
-          <Statistic text="average" value={average} />
-          <Statistic text="positive" value={positive_p} />
-        </table>
+      
+        <h1>Anecdote with most votes</h1>
+        <p>{anecdotes[max_point_index]}</p>
       </div>
+      
     )
   }
-
-
 }
 
-
-
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const [allOpinions, setAll] = useState({})
-  
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [p_, setp] = useState(0)
 
   const handler = (_state, _setter) => {
-    let f = () => { _setter(_state+1) }
-    return f
+    return () => {
+      if(_state+1 > anecdotes.length-1){
+        _setter(0)
+      } else {
+        _setter(_state+1)
+      } 
+    }
+    //return f
   }
 
-  const handleBadClick = handler(bad,setBad)
-  const handleGoodClick = handler(good,setGood)
-  const handleNeutralClick = handler(neutral,setNeutral)
-  
-  //const handleNeutralClick = () => {
-  //  //setAll(allClicks.concat('L'))
-  //  setNeutral(neutral+1)
-  //}
-  //const handleGoodClick = () => {
-  //  setGood(good+1)
-  //}
+  const handleNext = handler(selected,setSelected)
+  const handlePoints = () => {
+      points[selected]++;
+      setp(p_+1)
+  }
 
   return (
     <div>
-      <div>
-        <h1>Give Feedback</h1>
+      {props.anecdotes[selected]}<br />
+      has {props.points[selected]} votes<br />
+      <Button onClick={handleNext} text="Next" />
+      <Button onClick={handlePoints} text="Vote" />
 
-        <Button onClick={handleGoodClick} text="Good" />
-        <Button onClick={handleNeutralClick} text="Neutral" />
-        <Button onClick={handleBadClick} text="Bad" />
-
-        <Statistics bad={bad} neutral={neutral} good={good}/>
-      </div>
+      <br />
+     <Anecdotes />
     </div>
-    
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const points = [0,0,0,0,0,0]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} points={points} />,
+  document.getElementById('root')
+)
