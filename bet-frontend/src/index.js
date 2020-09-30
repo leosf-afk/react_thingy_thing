95526f7288bed18ea7153870140eeb5a934cb98f
jnrdrgz <jnigrodriguez@gmail.com>
2020-09-30 01:25:12 -0300

    edit match result added and balance sync

--- bet-frontend/src/index.js
+++ bet-frontend/src/index.js
@@ -35,8 +35,11 @@ const matchReducer = (state = [], action) => {
     case "NEW_MATCH":
       return [...state, action.data]
     case "GET_MATCHES":
-        lg("MATCH REDUCER - GET MATCHES", action.data)
-        return action.data
+      lg("MATCH REDUCER - GET MATCHES", action.data)
+      return action.data
+    case "EDIT_MATCH":
+      lg("MATCH REDUCER -  EDIT_MATCH", action.data)
+      return state
     default: 
       return state
   }
@@ -50,16 +53,16 @@ const ticketReducer = (state = [], action) => {
     case "GET_TICKETS":
       lg("TICKETS REDUCER - GET TICKETS", action.data)
       return action.data
-
+      
     default: 
       return state
   }
 }
 
-const tillReducer = (state = [], action) => {
+const tillReducer = (state = 0, action) => {
   switch(action.type){
     case "GET_TILL_AMOUNT":
-      return state
+      return action.amount
     case "UPDATE_TILL_AMOUNT":
       return state
       
@@ -80,6 +83,18 @@ const addMatch = (data) => {
   }
 }
 
+const editMatchResult = (data) => {
+  lg("EDIT REQ", data)
+  return async dispatch => {
+    const res = await api.put('/matches/edit',  data);
+    
+    dispatch({
+      type: "EDIT_MATCH",
+      data: res.data
+    })
+  }
+}
+
 const getMatches = () => {
     return async dispatch => {
     const res = await api.get("/matches")
@@ -96,6 +111,18 @@ const getMatches = () => {
   }
 }
 
+const getTill = () => {
+  return async dispatch => {
+    const res = await api.get("/till")
+    lg("GET TILL", res)
+    dispatch({
+      type: "GET_TILL_AMOUNT",
+      amount: res.data.data.amount
+    })
+  }
+}
+
+
 //////////ticket actions
 const addTicket = (data) => {
   return async dispatch => {      
@@ -111,22 +138,11 @@ const addTicket = (data) => {
 
 const getTickets = () => {
   return async dispatch => {
-  const res = await api.get("/tickets")
-  lg("GET TICKETS", res)
-  dispatch({
-    type: "GET_TICKETS",
-    data: res.data.tickets
-  })
-}
-}
-
-const updateMatchResult = (data) => {
-  return async dispatch => {
-    const res = await api.put('/matches/update',  data);
-    
+    const res = await api.get("/tickets")
+    lg("GET TICKETS", res)
     dispatch({
-      type: "UPDATE_MATCH",
-      data: res.data.match
+      type: "GET_TICKETS",
+      data: res.data.tickets
     })
   }
 }
@@ -137,9 +153,39 @@ const appReducer = combineReducers({
     till: tillReducer,
 })
 
-const Ticket = (props) => {
-   lg("!!!!!!!!!!!!!!!!!!", props)
+let PayButton = (props) => {
   
+  switch(props.ticket_result){
+    case "P":
+      return(
+        <div>Pending</div>
+      )
+    case "W":
+      return(
+        <div>
+          <button>PAY</button>
+        </div>
+      )
+    case "L":
+      return(
+        <div>Lost</div>
+      )
+    case "N":
+      return(
+        <div>Nulled</div>
+      )
+    default:  
+      return(
+       <div>Error</div>
+      )
+  }
+}
+
+PayButton = connect()(PayButton)
+
+
+
+const Ticket = (props) => {
     const get_posible_price = () => {
       switch(props.data.bet){
         case "one":
@@ -157,7 +203,12 @@ const Ticket = (props) => {
            match: {props.data.team1}-{props.data.team2}<br />
            bet: {props.data.bet} match result: {props.data.match_result}<br />
            amount: ${props.data.amount} posible price: ${get_posible_price()}<br />
-           state: {props.data.state} <br />
+           state: {props.data.state} <br /> 
+           <PayButton 
+            ticket_result={props.data.ticket_result}
+            ticket_id={props.data.id}
+            />
+           
            <br /><br />
         </div>
     )
@@ -171,7 +222,6 @@ let ViewTickets = (props) => {
 
     lg("IN VIEW TICKERS", props)
 
-    
     return (
         <div style={_style}>
             <h2>tickets:</h2>
@@ -250,44 +300,92 @@ AddTicket = connect(
   {addTicket}
 )(AddTicket)
 
+let EditMatchResult = (props) => {
+  const comp_name=`result-${props.id}`;
+  
+  const _editMatch = async (event) => {
+    const data = {
+      id: props.id,
+      result: event.target[comp_name].value,
+    }
+    
+    lg("EDITMATCH", data)
+    props.editMatchResult(data)
+  }
+  
+  
+  return (
+    <div>
+      <form onSubmit={_editMatch}>
 
-const Match = ({team1, team2, odds}) => {
+        <select style={formInputStyle} name={comp_name} id="result">            
+            <option value="-">-</option>
+            <option value="one">1</option>
+            <option value="two">2</option>
+            <option value="x">X</option>
+        </select>
+
+        <button style={formButtonStyle} type="submit">Edit</button>
+      </form>
+    </div>
+
+  )
+
+}
+
+EditMatchResult = connect(
+  null, 
+  {editMatchResult}
+)(EditMatchResult)
+
+
+const Match = ({id, result, team1, team2, odds, edit_mode}) => {
     console.log(odds)
-    return (
+    if(!edit_mode){
+      return (
+          <div>
+              {team1} - {team2} result: {result}
+              <br />
+                1: {odds.one} - x: {odds.x} - 2: {odds.two}
+              <br />
+
+          </div>
+      )
+    } else {
+      return (
         <div>
-            {team1} - {team2} 
-            <select>
-                <option value="1">1</option>
-                <option value="X">X</option>
-                <option value="2">2</option>
-            </select>
-            <br />
-              1: {odds.one} - x: {odds.x} - 2: {odds.two}
-            <br />
-            
+            {team1} - {team2} <EditMatchResult id={id} result={result} />
         </div>
-    )
+      )
+    }
 }
 
 let ViewMatches = (props) => {
     //console.log(props)
     lg("VIEW MATCHES", props)
     const {matches} = props
-    //return (<div>ahre</div>)
+    const [ editMode, setEditMode ] = useState(false)
+    matches.edit_mode = editMode;
+
     const _style = {
       width: "50%",
       backgroundColor: "#f9d56e",
     }
+
     if(matches){
       return (
         <div style={_style}>
+            <button onClick={() => setEditMode(!editMode)}>edit</button>
             <h2>Matches:</h2>
             {matches.map(match =>
               <Match
                 key={match.id}
+                id={match.id}
+                result={match.result}
                 team1={match.team1}
                 team2={match.team2}
                 odds={match.Odd}
+                edit_mode={editMode}
               />
             )}
             
@@ -308,6 +406,7 @@ const mapStateToViewMatchesProps = (state) => {
   }
 }
 ViewMatches = connect(mapStateToViewMatchesProps, null)(ViewMatches)
+
 
 let AddMatch = (props) => {
     lg("PROPS_ADDMATCH", props)
@@ -353,6 +452,7 @@ let AddMatch = (props) => {
             <button style={formButtonStyle} type="submit">Add</button>
             </form>
 
+
         </div>
     )
 }
@@ -380,8 +480,6 @@ let MatchesDropDown = (props) => {
             </option>)}
             
             </select>
-            
-          
       </div>
     )
   } else {
@@ -401,20 +499,36 @@ MatchesDropDown = connect(mapStateToMatchesDropDownProps, null)(MatchesDropDown)
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-const Balance = () => {
-    
+let Balance = (props) => {
+  const _style = {
+    width: "50%",
+    backgroundColor: "#f9d56e",
+  }
+   
     return (
-        <div>
-            Balance: 0
-        </div>
+      <div style={_style}>
+          Balance: ${props.amount}
+      </div>
     )
 }
+
+const mapStateToBalanceProps = (state) => {
+  
+  lg("MAP BALANCE VIEW", state)
+  return {
+    amount: state.till
+  }
+}
+Balance = connect(mapStateToBalanceProps, null)(Balance)
+
+
 
 const App = () => {
     const dispatch = useDispatch();
     useEffect(() => {
       dispatch(getMatches())
       dispatch(getTickets())
+      dispatch(getTill())
     },[dispatch]) 
 
     return (
